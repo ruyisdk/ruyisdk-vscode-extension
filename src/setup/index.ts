d442a4c73dd44cea8f7b1823a67cd13c06d45593
iex123 <i_seek-you@foxmail.com>
2025-11-29 22:03:14 +0800

    refactor(telemetry): restructure telemetry handling and integrate into setup module
    Signed-off-by: iex123 <i_seek-you@foxmail.com>

--- src/setup/index.ts
+++ src/setup/index.ts
@@ -4,6 +4,8 @@ import * as vscode from 'vscode'
 import { registerDetectCommand, registerManageCommand } from './manage.command'
 import { manageService } from './manage.service'
 import { registerInstallCommand, registerUpdateCommand } from './setup.command'
+import registerTelemetryCommand from './telemetry.command'
+import { telemetryService } from './telemetry.service'
 
 export default function registerSetupModule(ctx: vscode.ExtensionContext): void {
   // Register commands
@@ -11,8 +13,11 @@ export default function registerSetupModule(ctx: vscode.ExtensionContext): void 
   registerManageCommand(ctx)
   registerInstallCommand(ctx)
   registerUpdateCommand(ctx)
+  registerTelemetryCommand(ctx)
 
   // Initialize manage service
   manageService.initialize(ctx)
   ctx.subscriptions.push(manageService)
+  // Initialize telemetry service
+  ctx.subscriptions.push(telemetryService)
 }
